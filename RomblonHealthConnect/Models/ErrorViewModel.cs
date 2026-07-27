506bff3f063848c04f2f72d409648d8d63102964
Jayvee Molino <jayvmolino@gmail.com>
2026-07-28 00:32:49 +0800

    Project Structure

---
+++ RomblonHealthConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RomblonHealthConnect.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
